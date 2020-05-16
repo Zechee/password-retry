e1b4a9b4d7fddaf17886a63d5e83f6cd0fd293db
Nathan Yin <yinzeqi11@gmail.com>
2020-05-16 19:36:44 +1200

    Change to while > 0

--- pwd.py
+++ pwd.py
@@ -1,17 +1,17 @@
 password = 'a12345' #密码
 time = 4 #密码的尝试次数
 
-while True:
+while time > 0:
 	pwd = input('请输入密码： ')
+	time = time - 1
+	
 	if pwd == password:
 		print('密码正确，登陆成功！')
 		break
 
 	else:
-		time = time - 1
-		if time < 1:
-			print('你已经没有尝试机会了！')
-			break
-
-		else:
+		print('密码错误')
+		
+		if time > 0:
 			print('请重新尝试，你还有', time, '次机会')
+
